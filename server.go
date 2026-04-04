64c74b33cb01b0bf1b9f7ee0ae8b2d37ed3e7a53
Paul Vinueza <paulmv@Georges-MacBook-Pro.local>
2026-04-04 04:56:44 -0400

    refactor fmt to log for logging

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"os"
@@ -47,6 +48,8 @@ func (s *server) start() error {
 	if err != nil {
 		return err
 	}
+	tcpAddr := ln.Addr().(*net.TCPAddr)
+	log.Printf("Linko is running on http://localhost:%d\n", tcpAddr.Port)
 	if err := s.httpServer.Serve(ln); !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
@@ -54,6 +57,7 @@ func (s *server) start() error {
 }
 
 func (s *server) shutdown(ctx context.Context) error {
+	log.Println("Linko is shutting down")
 	return s.httpServer.Shutdown(ctx)
 }
 
