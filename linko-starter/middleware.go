7bb720fd0762edfcdce7eb2ea7e133459ffef368
Paul Vinueza <paulvinueza30@gmail.com>
2026-04-13 23:07:13 -0400

    add: middleware for getting request id from request headers

--- linko-starter/middleware.go
+++ linko-starter/middleware.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"io"
 	"log/slog"
 	"net/http"
 	"time"
 )
 
-const logContextKey contextKey = "log_context"
+const (
+	logContextKey contextKey = "log_context"
+	reqIDHeader   string     = "X-Request-ID"
+)
 
 type LogContext struct {
 	Username string
@@ -66,11 +70,13 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 			r = r.WithContext(ctx)
 
 			next.ServeHTTP(spyWriter, r)
+			reqID := r.Context().Value("request_id").(string)
 			attrs := []any{
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
 				slog.String("client_ip", r.RemoteAddr),
 				slog.Duration("duration", time.Since(start)),
+				slog.String("request_id", reqID),
 				slog.Int("request_body_bytes", spyReader.bytesRead),
 				slog.Int("response_status", spyWriter.statusCode),
 				slog.Int("response_body_bytes", spyWriter.bytesWritten),
@@ -85,6 +91,21 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 				attrs = append(attrs, slog.Any("error", httpError))
 			}
 			logger.Info("Served request", attrs...)
+		})
+	}
+}
+
+func requestID() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			reqID := r.Header.Get(reqIDHeader)
+			if reqID == "" {
+				reqID = rand.Text()
+			}
+			w.Header().Set(reqIDHeader, reqID)
+			ctx := context.WithValue(r.Context(), "request_id", reqID)
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
 		})
 	}
 }
